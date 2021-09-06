2e92ec2ce6b08c1b0ab27b2378a47586b7cdbb84
oioi-ioi <stefan.frank@gmx.at>
2021-09-06 12:15:31 +0200

    this should be the third commit now

--- some_functions.py
+++ some_functions.py
@@ -3,3 +3,9 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n - 1)    
+
+def fibonacci(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return fibonacci(n - 2) + fibonacci(n - 1)    
