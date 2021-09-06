ef0fd39dd9992adb1e3e4b56661329192ce1a605
oioi-ioi <stefan.frank@gmx.at>
2021-09-06 22:59:36 +0200

    just added a comment and the blabla function

--- my_first_module/some_functions_in_first_module.py
+++ my_first_module/some_functions_in_first_module.py
@@ -1,2 +1,9 @@
 def print_bla():
     print('bla')
+
+#from .some_functions import fibonacci   
+# 
+# check how relative imports work at https://docs.python.org/3/tutorial/modules.html 
+
+def print_blabla():
+    print('blabla')
