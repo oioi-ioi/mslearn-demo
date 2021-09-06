17091a2ee4f72c5466b37f93ab94afa49c64f375
oioi-ioi <stefan.frank@gmx.at>
2021-09-06 15:16:26 +0200

    fifth try

--- some_functions.py
+++ some_functions.py
@@ -8,4 +8,7 @@ def fibonacci(n):
     if n == 0 or n == 1:
         return 1
     else:
-        return fibonacci(n - 2) + fibonacci(n - 1)    
+        return fibonacci(n - 2) + fibonacci(n - 1)   
+
+def do_nothing():
+    pass
