7bd517a00b1003e20ddcb29bad741c9f94e629ff
oioi-ioi <stefan.frank@gmx.at>
2021-09-06 15:42:13 +0200

    This is the first attempt to commit after i realized that i have to push the commit afterwards

--- some_functions.py
+++ some_functions.py
@@ -10,5 +10,10 @@ def fibonacci(n):
     else:
         return fibonacci(n - 2) + fibonacci(n - 1)   
 
-def do_nothing():
-    pass
+def fibonacci_like(n):
+    if n == 0 or n == 1:
+        return 1
+    elif n == 2:
+        return 3    
+    else:
+        return fibonacci_like(n - 3) + fibonacci_like(n - 2) + fibonacci_like(2 - 1)   
